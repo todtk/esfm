f64a906af566677396a34e02f406f992f61af9c1
todtk <batkatod#gmail.com>
2023-03-11 19:59:10 +0500

    add signals + logic for statusbar

--- scripts/data.py
+++ scripts/data.py
@@ -3,7 +3,7 @@
 import os
 from io import BytesIO
 from itertools import cycle
-from .files import *
+from .files import open_file, open_memory, write_file
 
 
 class DataManager():
@@ -30,43 +30,46 @@ class DataManager():
             ""
         )
 
-        self.success = 0
-        self.errors = 0
-
     ####################################################################################################
     ### DECODER                                                                                      ###
     ####################################################################################################
-    def check_datapack_file(self) -> bool:
+    def check_data1(self) -> bool:
         return os.path.exists(self.pack_path)
 
     def get_xor(self, data: bytearray, key: bytearray) -> bytes:
         return bytes(a ^ b for a, b in zip(data, cycle(key)))
     
-    def get_decode_datapack(self) -> bool:
+    def get_decode(self, signal) -> None:
         try:
 
             key = b'\x21\x0c\xed\x10\xd8\x81\xd7\xa3\xfa\x9b\xc9\x7a\xd3\xae\xeb\x6d\x98\x89\x31\x34\x2d\x39\x1e\x1f\xe1\xc4\x7c\xdd\x2d\xef\x26\x37\x7a\xfa\xbf\xd2\xd9\x60\x79\xf1\xca\x99\xd0\x32\xf7\xd8\x4d\x4e\xf6\xce\x45\xda\x0c\x67\x99\x09\xe6\x89\x75\x69\x5f\xd9\x12\xa2\x3e\x77\x74\x3c\xf5\xbe\x2e\x57\x64\x05\x1a\x71\x96\x62\x23\x25\x80\x63\xfc\xe7\xc6\xd4\xe7\xca\x76\x7d\x70\x3c\xcb\xe2\x31\xc5\xed\x03\x8d\xcc\xad\x1a\x75\x53\x4a\x61\x27\xb8\x30\xca\xeb\x73\xb4\xc6\xd6\xdb\xda\x00\x88\xe2\x11\x21\xef\xd5\xf3\x8a\x02\x1f\x06'
             stream = BytesIO(open(self.pack_path, "rb").read())
+            
+            size = os.path.getsize(os.path.join(self.pack_path))
+            cur_persent = 0
 
             with open(self.pck_path, "wb") as pck:
 
                 while True:
+
+                    persent = int(os.path.getsize(os.path.join(self.pck_path)) / (size / 100))
+                    if cur_persent != persent:
+                        signal.emit(persent)
+                        cur_persent = persent
 
                     decrypted_bytes = self.get_xor(stream.read(len(key)), key)
                     pck.write(decrypted_bytes)
 
                     if decrypted_bytes == b'':
                         break
-                    
-                return True
-            
+
         except:
-            return False
+            ...
 
     ####################################################################################################
     ### EXTRACTOR                                                                                    ###
     ####################################################################################################
-    def check_datapck_file(self) -> bool:
+    def check_data2(self) -> bool:
         return os.path.exists(self.pck_path)
 
     def read_header(self, header: bytes) -> int:
@@ -96,18 +99,25 @@ class DataManager():
         else:
             return 0, 0, 0
 
-    def get_extract_datapck(self) -> bool:
+    def get_extract(self, signal) -> None:
 
         pck_file = open_file(
             file_path = self.pck_path,
             write=False
         )
 
+        size = os.path.getsize(os.path.join(self.pck_path))
+
         mfile = open_memory(
             file = pck_file,
-            length = os.path.getsize(os.path.join(self.pck_path))
+            length = size
         )
 
+        # counters
+        success = 0
+        errors = 0
+
+        # cursors
         cur = 0
         flag = 0
 
@@ -115,6 +125,12 @@ class DataManager():
 
             if cur >= flag:
                 flag = cur
+
+                status_dict = {
+                    "persent": int(cur / (size / 100)),
+                    "success:errors": [success, errors]
+                }
+                signal.emit(status_dict)
 
                 try:
 
@@ -137,19 +153,17 @@ class DataManager():
 
                             status = write_file(path=full_path, container=container)
                             if status:
+                                success += 1
                                 print("EXTRACTED: ", full_path)
 
                             cur += 4 + length_path + length_container + 4
-                            self.success += 1
                             continue
 
                 except:
-                    self.errors += 1
+                    errors += 1
 
                 cur += 1
                 continue
 
             else:
                 break
-        
-        return True
