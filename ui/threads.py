2f8fb8950ffb021a93777244998f607f5d0d5350
todtk <batkatod#gmail.com>
2023-03-11 20:05:54 +0500

    add imports, class examples, signals for Data...Thread classes

--- ui/threads.py
+++ ui/threads.py
@@ -1,26 +1,31 @@
 # -*- coding: utf-8 -*-
 
 from PyQt5 import QtCore
+from scripts import data
 
 
 class DataDecodeThread(QtCore.QThread):
-    mysignal = QtCore.pyqtSignal(dict)
+    mysignal = QtCore.pyqtSignal(int)
 
-    def __init__(self, data, parent = None) -> None:
+    def __init__(self, parent = None) -> None:
         QtCore.QThread.__init__(self, parent = parent)
 
+        self.data = data.DataManager()
+
     def run(self):
-        ...
+        self.data.get_decode(self.mysignal)
 
 
 class DataExtractThread(QtCore.QThread):
     mysignal = QtCore.pyqtSignal(dict)
 
-    def __init__(self, data, parent = None) -> None:
+    def __init__(self, parent = None) -> None:
         QtCore.QThread.__init__(self, parent = parent)
         
+        self.data = data.DataManager()
+
     def run(self):
-        ...
+        self.data.get_extract(self.mysignal)
 
 
 class TempExtractThread(QtCore.QThread):
