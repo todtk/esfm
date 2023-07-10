c5c1f9579503e3d1f8eb4862c38ce4faf54586b4
todtk <batkatod@gmail.com>
2023-07-10 09:46:01 +0500

    small changes, update ver 0.02 -> 0.04

--- main.py
+++ main.py
@@ -2,29 +2,26 @@
 
 import os
 import sys
-import scripts
 import pathlib
 from PyQt5 import QtWidgets
 from ui.window import AppWindow
 
 
-class ESFM:
-    __version__: float = 0.02
+class ESFM():
+    __version__: float = 0.04
 
     def __init__(self) -> None:
-        pathlib.Path(os.path.join(os.getcwd(), "client", "")).mkdir(parents=True, exist_ok=True)
-        pathlib.Path(os.path.join(os.getcwd(), "logs", "")).mkdir(parents=True, exist_ok=True)
-
-        data = scripts.DataManager()
-        # temp = scripts.TempManager()
-        # cache = scripts.CacheManager()
 
         self.app = QtWidgets.QApplication(sys.argv)
-        self.window = AppWindow(self.__version__, data)
+        self.window = AppWindow(self.__version__)
 
-
-esfm = ESFM()
 
 if __name__ == "__main__":
+
+    pathlib.Path(os.path.join(os.getcwd(), 'client', 'cache')).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(os.path.join(os.getcwd(), 'client', 'files', 'temp')).mkdir(parents=True, exist_ok=True)
+
+    esfm = ESFM()
+
     esfm.window.show()
     sys.exit(esfm.app.exec_())
